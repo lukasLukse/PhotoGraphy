7ab084898f64dea87106c11151b85c48d8ccae1a
unknown <lukas.lasas1@gmail.com>
2024-11-02 23:59:38 +0200

    Updated styles

---
+++ pages/album/index.tsx
@@ -0,0 +1,16 @@
+import PageTemplate from "@/components/PageTemplate/PageTemplate";
+import styles from "./styles.module.css";
+
+const index = () => {
+  return (
+    <>
+      <PageTemplate>
+        <div className={styles.main}></div>
+      </PageTemplate>
+    </>
+  );
+};
+
+export default index;
+
+// Padaryti dizaina, sudėti daugiau gražių fotkių, padaryt slankiojančias fotkes//
